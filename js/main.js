66b52a06d13ad99d8eb2a8eb1ae10321e9d52879
liker2510 <81087122+liker2510@users.noreply.github.com>
2021-05-28 15:28:05 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar');
+const closeMenuButton = document.querySelector('.close-bt ');
+const toggleMenu = (event) => {
+    menu.classList.toggle('is-open');
+};
+menuButton.addEventListener('click', toggleMenu);
+closeMenuButton.addEventListener('click', toggleMenu);
